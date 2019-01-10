cbc7bffec60b9bba400f06d12097c04fe4d122a5
unknown <varun.kamarapu@gmail.com>
2019-01-10 19:48:08 +0000

    More css Sytling 2

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-    socket.emit('newMessage', generateMessage(message.from, message.text));
+    io.emit('newMessage', generateMessage(message.from, message.text));
     callback('From Server');
 
   });
